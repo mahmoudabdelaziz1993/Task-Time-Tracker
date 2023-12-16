1b26977f5cd534f5247a0c06d358785e1c4d33b5
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2023-12-16 04:46:22 +0200

    Login page from shadcn Examples

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Titillium_Web } from 'next/font/google'
+import '@/styles/globals.css'
+import { cn } from '@/lib/utils'
 
-const inter = Inter({ subsets: ['latin'] })
+const genos = Titillium_Web({ subsets: ['latin',"latin-ext"]  , variable: "--font-genos" , weight: "400"  })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+          "min-h-screen bg-background font-genos antialiased",
+          genos.variable
+        )}>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -70,6 +72,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        genos: ["var(--font-genos)", ...fontFamily.sans],
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
